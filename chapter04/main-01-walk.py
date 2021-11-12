dab66d0048f1ce4a8a825b4d7ee6b75142ce6959
yauheni <sarokin.yauheni@gmail.com>
2021-11-12 10:02:51 +0200

    chapter 4 started

---
+++ chapter04/main-01-walk.py
@@ -0,0 +1,20 @@
+# random walk, page 75
+from vpython import *
+
+import random
+
+random.seed(None)
+jmax = 200
+x = 0.
+y = 0.
+
+scene = canvas(title='Random walk',
+     width=500, height=500,
+     center=vector(0,0,0), background=color.cyan)
+
+c = curve(vector(-1,-1,0), vector(1,-1,0))
+for i in range(0, jmax + 1):
+    rate(10)
+    c.append(vector(x, y, 0))
+    x += (random.random() - 0.5) * 2
+    y += (random.random() - 0.5) * 2
---
+++ chapter04/test-vpython.py
@@ -0,0 +1,3 @@
+from vpython import *
+
+sphere(pos=vector(-1, 0, 0), radius=0.5, color=color.green)
